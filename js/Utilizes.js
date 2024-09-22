e36b00ccfa5e2914ac8f35f42c5bbf7985bb1449
MeghlaB <meghlab964@gmail.com>
2024-09-22 23:21:00 +0600

    frist button js added

--- js/Utilizes.js
+++ js/Utilizes.js
@@ -1,14 +1,42 @@
-// dontae And History button
- function getFiledBtn(id){
-    const btn=document.getElementById(id).innerText
-    btn.classList.add=(
-        "px-14 py-2 text-2xl bg-gradient-to-r from-blue-600 to-purple-600 mt-4 rounded-l-x"
-    )
-    //history  btn remove
-    document.getElementById("historyBtn").classList.remove(
-        "px-14 py-2 text-2xl bg-gradient-to-r from-blue-600 to-purple-600 mt-4 rounded-l-x"
-    )
 
- }
+
+//donate btn ar function create
+function getInputFiled(id){
+    const text = parseFloat(document.getElementById(id).value)
+    return text
+}
+
+//popUp btn ar jonno
+
+function getFiledBtn(id){
+    const btn = document.getElementById(id)
+    return btn
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // dontae And History button
+//  function getFiledBtn(id){
+//     const btn=document.getElementById(id).innerText
+//     btn.classList.add=(
+//         "px-14 py-2 text-2xl bg-gradient-to-r from-blue-600 to-purple-600 mt-4 rounded-l-x"
+//     )
+//     //history  btn remove
+//     document.getElementById("historyBtn").classList.remove(
+//         "px-14 py-2 text-2xl bg-gradient-to-r from-blue-600 to-purple-600 mt-4 rounded-l-x"
+//     )
+
+//  }
 
  
--- js/index.js
+++ js/index.js
@@ -1,6 +1,33 @@
-// Donate and history btn call
-const btn=getFiledBtn("donationBtn")
 
+
+//donate btn ay clik kory inputFiled acces hbe
+
+document.getElementById("fristDonateBtn" ).
+addEventListener("click",function(){
+    const text = getInputFiled("fristInputFiled")
+    const Blance = parseFloat(document.getElementById("fristBlance").innerText)
+        const subsblance= Blance - text
+        document.getElementById("fristBlance").innerText=subsblance
+    
+        const amount =  parseFloat(document.getElementById("Amount").innerText)
+        const totalBlance = text + amount
+    
+        document.getElementById("Amount").innerText = totalBlance
+    
+       const vidssible = getFiledBtn("congrast")
+       vidssible.className = "absolute top-18 left-18 w-[350] lg:w-[500px] h-[300px] mx-auto border border-2px bg-white drop-shadow rounded-2xl"
+       vidssible.classList.remove("hidden")
+   //close information
+
+   const closebtn= getFiledBtn("closeBtn")
+   closebtn.addEventListener("click",function(){
+    const vidssible = getFiledBtn("congrast")
+    vidssible.className = "absolute top-18 left-18 w-[350] lg:w-[500px] h-[300px] mx-auto border border-2px bg-white drop-shadow rounded-2xl"
+    vidssible.classList.add("hidden")
+   })
+
+ 
+})
 
 
 
