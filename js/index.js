0fb2a4c7bb5a0e721636f490c3780e898e3b8f0e
MeghlaB <meghlab964@gmail.com>
2024-09-24 01:03:40 +0600

    problemd sloved

--- js/index.js
+++ js/index.js
@@ -3,16 +3,21 @@
 document.getElementById("fristDonateBtn").
 addEventListener("click",function(){
     const text = getInputFiled("fristInputFiled")
+    document.getElementById("fristInputFiled").value ='';
     const Blance = parseFloat(document.getElementById("fristBlance").innerText)
+    const amount =  parseFloat(document.getElementById("Amount").innerText)
     const title = (document.getElementById("fristTitle").innerText)
-    if(isNaN(text) || text>Blance || text<=0){
+    if(isNaN(text) || text<=0){
        return alert("Invalid Input")
+    }
+    else if(text >= amount || amount <=text){
+        return alert("Invalid Input")
     }
     else{
     const subsblance= Blance + text
     document.getElementById("fristBlance").innerText=subsblance
 
-    const amount =  parseFloat(document.getElementById("Amount").innerText)
+  
     const totalBlance = amount - text
 
     document.getElementById("Amount").innerText = totalBlance
@@ -33,68 +38,87 @@ addEventListener("click",function(){
    historyItem.className = "bg-white shadow-sm border border-2px border-gray-200 rounded-2xl py-6 px-8 items-center gap-3"
 
    historyItem.innerHTML =`
-    <h1 class= "text-2xl text-black font-bold space-x-0 text-justify">${text.toFixed()} Taka is ${title}</h1>
+    <h1 class= "text-2xl text-black font-bold space-x-0 text-justify">${text.toFixed(2)} Taka is ${title}</h1>
     <p>Date ${new Date().toString()}</p>
    `
-   document.getElementById("historySection").appendChild(historyItem)
+    const historyContainar = document.getElementById("historySection")
+
+    historyContainar.insertBefore(historyItem, historyContainar.fristChild)
 })
 
 // Donate for Flood Relief in Feni,Bangladesh
 
 document.getElementById("secondBtn").addEventListener("click",function(){
     const text = getInputFiled("secondinputFiled")
+    document.getElementById("secondinputFiled").value ='';
     const Blance =parseFloat(document.getElementById("secondBlance").innerText)
+    const amount = parseFloat(document.getElementById("Amount").innerText)
     const title = document.getElementById("titleFiled").innerText
-    if(isNaN(text) || text>Blance){
+    if(isNaN(text)  || text<=0){
        return alert("Invalid Input")
     }
-    const totalBlance = Blance + text;
-    document.getElementById("secondBlance").innerText=totalBlance
-    const amount = parseFloat(document.getElementById("Amount").innerText)
-    const availbleBlance = amount - totalBlance 
-    document.getElementById("Amount").innerText=availbleBlance
-
-    const closeBtn = getFiledBtn("congrast1")
+    else if(text >= amount || amount <=text){
+        return alert("Invalid Input")
+    }
+    else{
+        const totalBlance = Blance + text;
+        document.getElementById("secondBlance").innerText=totalBlance
+      
+        const availbleBlance = amount - totalBlance 
+        document.getElementById("Amount").innerText=availbleBlance
     
-    closeBtn.className = "absolute top- left-18 w-[350] lg:w-[500px] h-[300px] mx-auto border border-2px bg-white drop-shadow rounded-2xl"
-    closeBtn.classList.remove("hidden")
-  
+        const closeBtn = getFiledBtn("congrast1")
+        
+        closeBtn.className = "absolute top- left-18 w-[350] lg:w-[500px] h-[300px] mx-auto border border-2px bg-white drop-shadow rounded-2xl"
+        closeBtn.classList.remove("hidden")
+        
+    }
     //close popUp remove
     document.getElementById("closeBtn1").addEventListener("click",function(){
         const removeBtn = getFiledBtn("congrast1")
         removeBtn.classList.add('hidden')
     })
+        
     const historyItem = document.createElement("div")
     historyItem.className = "bg-white shadow-sm border border-2px border-gray-200 rounded-2xl py-6 px-8 items-center "
- 
+
     historyItem.innerHTML =`
-     <h1 class= "text-2xl text-black font-bold space-x-0 text-justify">${text.toFixed()} Taka is ${title}</h1>
-     <p>Date ${new Date().toString()}</p>
+    <h1 class= "text-2xl text-black font-bold space-x-0 text-justify">${text.toFixed(2)} Taka is ${title}</h1>
+    <p>Date ${new Date().toString()}</p>
+
     `
-    document.getElementById("historySection").appendChild(historyItem)
-    
+    const historyContainar = document.getElementById("historySection")
+    historyContainar.insertBefore(historyItem, historyContainar.fristChild)
 })
 //Aid for Injured in the Quota Movement
 
 document.getElementById("thirdBtn").addEventListener("click",function(){
     const text = getInputFiled("inputFiled");
+    document.getElementById("inputFiled").value ='';
     const Blance = parseFloat(document.getElementById("Amount").innerText)
+    const amount = parseFloat(document.getElementById("Amount").innerText)
     const title = document.getElementById("thirdTitle" ).innerText
-    if(isNaN(text) || text>Blance){
+    if(isNaN(text) || text<=0){
         return alert ("Invalid Input")
     }
-    const totalBlance = Blance + text 
-    document.getElementById("thirdBlance").innerText = totalBlance
-
-    const amount = parseFloat(document.getElementById("Amount").innerText)
-    const Amount = amount - text 
-    document.getElementById("Amount").innerText = Amount
+    else if(text>=amount || amount <=text ){
+        return alert("Invalid Input")
+    }
+    else{
+        const totalBlance = Blance + text 
+        document.getElementById("thirdBlance").innerText = totalBlance
     
-    const remove = getFiledBtn("congrast2")
-    remove.className =(
-        "absolute top- left-18 w-[350] lg:w-[500px] h-[300px] mx-auto border border-2px bg-white drop-shadow rounded-2xl"
-    )
-    remove.classList.remove("hidden")
+       
+        const Amount = amount - text 
+        document.getElementById("Amount").innerText = Amount
+        
+        const remove = getFiledBtn("congrast2")
+        remove.className =(
+            "absolute top- left-18 w-[350] lg:w-[500px] h-[300px] mx-auto border border-2px bg-white drop-shadow rounded-2xl"
+        )
+        remove.classList.remove("hidden")
+    }
+
 
     //close popup hide
     document.getElementById("closeBtn2").
@@ -106,10 +130,12 @@ document.getElementById("thirdBtn").addEventListener("click",function(){
     historyItem.className = "bg-white shadow-sm border border-2px border-gray-200 rounded-2xl py-6 px-8 items-center "
  
     historyItem.innerHTML =`
-     <h1 class= "text-2xl text-black font-bold space-x-0 text-justify">${text.toFixed()} Taka is ${title}</h1>
+     <h1 class= "text-2xl text-black font-bold space-x-0 text-justify">${text.toFixed(2)} Taka is ${title}</h1>
      <p>Date ${new Date().toString()}</p>
     `
-    document.getElementById("historySection").appendChild(historyItem)
+    const historyContainar = document.getElementById("historySection")
+    historyContainar.insertBefore(historyItem, historyContainar.fristChild)
+
 })
 
 //History
@@ -120,10 +146,10 @@ historyBtn.addEventListener("click",function(){
     
     historyBtn.classList.add('bg-praimary')
     historyBtn.classList.remove('bg-white')
-    
     donationBtn.classList.remove('bg-praimary')
     donationBtn.classList.add('bg-white')
-
+    // document.getElementById("historySection").classList.remove("hidden")
+    document.getElementById("historySection").classList.remove("hidden")
     document.getElementById("cardSection").classList.add("hidden")
 })
 //dontation
@@ -135,4 +161,5 @@ donationBtn.addEventListener("click",function(){
     historyBtn.classList.remove('bg-praimary')
     historyBtn.classList.add('bg-white')
     document.getElementById("cardSection").classList.remove("hidden")
+    document.getElementById("historySection").classList.add("hidden")
 })
